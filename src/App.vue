3f1466b99271bcbe8e134213e8c7e4e4f5afd56f
papiricoh <slendeman0200@gmail.com>
2023-02-16 12:48:36 +0100

    Added fontasewome

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,15 @@ import HelloWorld from './components/HelloWorld.vue'
 <main>
     <div class="main_div">
       <div class="tabs_bar">
-        
+        <div class="tab_icon active">
+          <font-awesome-icon icon="fa-solid fa-house" />
+        </div>
+        <div class="tab_icon">
+          <font-awesome-icon icon="fa-solid fa-globe" />
+        </div>
+        <div class="tab_icon">
+          <font-awesome-icon icon="fa-solid fa-money-bill" />
+        </div>
       </div>
       <div>
         
